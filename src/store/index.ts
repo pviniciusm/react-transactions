3e90258c6b963a18f1b10c66d9417e4696923533
pviniciusm <pvinics@gmail.com>
2023-02-15 18:05:12 -0300

    remove log error with redux persist

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,30 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { persistedReducer } from "./rootReducer";
-import { persistStore } from "redux-persist";
+import {
+    persistStore,
+    FLUSH,
+    REHYDRATE,
+    PAUSE,
+    PERSIST,
+    PURGE,
+    REGISTER,
+} from "redux-persist";
 
 export const store = configureStore({
     reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
 });
 
 export const persistor = persistStore(store);
